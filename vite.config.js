b969013d1a0f2e78219f79588b503434379c4104
UBSIT_BAWA_AN <168501885+marcGil20234231@users.noreply.github.com>
2025-10-01 22:06:24 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/try' // relative paths so it can work on GH Pages; change to '/REPO_NAME/' if needed
+  base: '/try/' // relative paths so it can work on GH Pages; change to '/REPO_NAME/' if needed
 });
